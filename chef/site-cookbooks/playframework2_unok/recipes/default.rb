5275a8f5043c8778fa424db5b6e8039c667a0c32
Kazuhiko UNO <kazuhiko.uno@gmail.com>
2014-08-17 00:38:15 +0900

    typesafe activator 対応 JDK を openjdk に変更(oracle JDK の処理が正常に動かないため)

--- chef/site-cookbooks/playframework2_unok/recipes/default.rb
+++ chef/site-cookbooks/playframework2_unok/recipes/default.rb
@@ -75,3 +75,60 @@ node['playframework2_unok']['versions'].each do |version|
 end
 
 
+node['playframework2_unok']['activator_versions'].each do |version|
+  if ::Dir.exist?("#{node['playframework2_unok']['play_dir']}/activator-#{version}")
+    next
+  end
+
+  directory node['playframework2_unok']['play_dir'] do
+    owner node['playframework2_unok']['user']
+    group node['playframework2_unok']['group']
+    mode "0775"
+    action :create
+  end
+
+  version_data = version.split('.')
+  cache_path = Chef::Config[:file_cache_path]
+  zip_file_url = ''
+  zip_file_name = "typesafe-activator-#{version}.zip"
+  zip_file_url = "http://downloads.typesafe.com/typesafe-activator/#{version}/typesafe-activator-#{version}.zip"
+
+  execute "echo" do
+    command "echo #{zip_file_url}"
+  end
+
+  remote_file "#{cache_path}/#{zip_file_name}" do
+    source zip_file_url
+    mode "0644"
+  end
+
+  bash "extract activator zip" do
+    cwd node['playframework2_unok']['play_dir']
+    code <<-BASH
+    unzip #{cache_path}/#{zip_file_name}
+    chown -R #{node['playframework2_unok']['user']}:#{node['playframework2_unok']['group']} .
+    BASH
+  end
+
+
+  template "#{node['playframework2_unok']['play_dir']}/activator" do
+    not_if do
+      ::File.exist?("#{node['playframework2_unok']['play_dir']}/activator")
+    end
+    source "activator.erb"
+    owner node['playframework2_unok']['user']
+    group node['playframework2_unok']['group']
+    mode "0775"
+    variables(
+        :current_version => version
+    )
+  end
+
+  bash "add bash path" do
+    not_if "which activator"
+    code <<-BASH
+      echo "export PATH=#{node['playframework2_unok']['play_dir']}:$PATH" >> /etc/bashrc
+    BASH
+  end
+end
+
